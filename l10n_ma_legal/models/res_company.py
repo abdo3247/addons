c0a41d0ce86498113526eb114007d104739d431f
Saidi <abdo4766@gmail.com>
2018-11-25 00:45:53 +0100

    Update res_company.py

--- l10n_ma_legal/models/res_company.py
+++ l10n_ma_legal/models/res_company.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class ResCompany(models.Model):
     _inherit = 'res.company'
     
-    pat = fields.Char(string='Patente')
+    tp = fields.Char(string='TP')
     ice = fields.Char(string='ICE')
     cnss = fields.Char(string='CNSS')
 
